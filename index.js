5a9befea76a4705972c4b647b9b89ad2466da1b4
AN <aurimas@clubneno.com>
2025-09-15 09:54:08 +0300

    Add index.js for static file serving and update Vercel config

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle all routes by serving index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+module.exports = app;
